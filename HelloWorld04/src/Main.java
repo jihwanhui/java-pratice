adb0888584297f35702f9d2e63165b0af4d5a639
hwanhui <ghh793507@gmail.com>
2025-04-08 11:21:53 +0900

    hi

--- HelloWorld04/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       String name = "서진환";
-       int age = 24;
-       System.out.println(name + "님은" + age + "살 입니다.");
-    }
-}
